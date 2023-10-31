fc0f358cd58ae85bc941001a0bf8eea4b70e3ed6
theDEveloper200 <138037265+theDEveloper200@users.noreply.github.com>
2023-10-31 12:56:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*Js for class = location*/
+// Js for class = location
 const seemorebtn = document.querySelector(".see-more-btn");
 const subloc = document.querySelector(".sub-loc");
 const location1 = document.querySelector(".location-1");
@@ -14,7 +14,7 @@ seemorebtn.addEventListener("click", () => {
   }
 });
 
-/*Js for class = down2-section*/
+// Js for class = down2-section
 const dosc21 = document.querySelector(".dosc21");
 const dosc210 = document.querySelector(".dosc210");
 
@@ -24,14 +24,14 @@ dosc21.addEventListener("click", () => {
   dosc21.classList.toggle("doosc21");
 });
 
-/*Js for login page */
+// Js for login page
 function showModel() {
-  document.querySelector(".container").classList.add("hideContainer");
+  document.querySelector(".container").classList.add("darkContainer");
   document.querySelector(".login-form").classList.add("showlogin-form");
 }
 
 function closeModel() {
-  document.querySelector(".container").classList.remove("hideContainer");
+  document.querySelector(".container").classList.remove("darkContainer");
   document.querySelector(".login-form").classList.remove("showlogin-form");
 }
 
@@ -41,7 +41,47 @@ lbtn.addEventListener("click", showModel);
 var c = document.querySelector(".m");
 c.addEventListener("click", closeModel);
 
-/*Js for password show */
+var viewmore = document.querySelector(".view-more")
+viewmore.addEventListener("click", closeModel);
+
+// Js for view-section
+function showBox(){
+  document.querySelector(".container").classList.add("hideContainer");
+  document.querySelector(".view-section").classList.add("showview-section");
+}
+function closeBox() {
+  document.querySelector(".container").classList.remove("hideContainer");
+  document.querySelector(".view-section").classList.remove("showview-section");
+}
+
+var viewmore = document.querySelector(".view-more");
+viewmore.addEventListener("click", showBox);
+
+var d = document.querySelector(".x");
+d.addEventListener("click", closeBox);
+
+var container = document.querySelector(".container")
+container.addEventListener("click", closeBox)
+ 
+//Js for dark mode
+const dkm = document.querySelector(".Dkm");
+const background = document.querySelector(".background");
+const vmsec = document.querySelector(".vm-sec");
+const down3section =document.querySelector(".down3-section");
+const sdkm = document.querySelector(".s-dkm");
+
+dkm.addEventListener("click" , () => {
+document.body.classList.toggle("dark-theme");
+location1.classList.toggle("dark-location1");
+down3section.classList.toggle("dark-down3-section");
+vmsec.classList.toggle("dark-vm-sec");
+sdkm.classList.toggle("shift-s-dkm");
+
+
+}
+)
+
+// Js for password show 
 let password = document.getElementById("password");
 let eyeicon = document.getElementById("eye-icon");
 
@@ -54,3 +94,4 @@ eyeicon.onclick = function () {
     eyeicon.src = "img/close.icon.png";
   }
 };
+
